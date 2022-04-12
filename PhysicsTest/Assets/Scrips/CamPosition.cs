e85835a09259272a781030243f9414250b57ec3e
Ramiro <rami.guzmi@gmail.com>
2022-04-12 11:34:30 -0300

    changes

---
+++ PhysicsTest/Assets/Scrips/CamPosition.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamPosition : MonoBehaviour
+{
+    List<GameObject> platforms = new List<GameObject>();
+    void Awake()
+    {
+        platforms.AddRange(GameObject.FindGameObjectsWithTag("Plataform"));
+    }
+    private void Start()
+    {
+        for (int i = 0; i < platforms.Count - 1; i++)
+        {
+            GameObject aux;
+            if (platforms[i].transform.position.y < platforms[i + 1].transform.position.y)
+            {
+                aux = platforms[i];
+                platforms[i] = platforms[i + 1];
+                platforms[i + 1] = platforms[i];
+            }
+        }
+        for (int i = 0; i < platforms.Count; i++)
+        {
+            Debug.Log(platforms[i].transform.position.y);
+        }
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ PhysicsTest/Assets/Scrips/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody Rg;
+
+    [SerializeField] float horizontalSpeed;
+    [SerializeField] float verticalSpeed;
+    [SerializeField] float jumpForce;
+
+    float h;
+    float v;
+    private void Awake()
+    {
+        Rg = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        if (isGrounded())
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Rg.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            }
+        }
+
+    }
+    private void FixedUpdate()
+    {
+
+        Rg.AddForce(h * horizontalSpeed, 0, v * verticalSpeed);
+
+    }
+
+    bool isGrounded()
+    {
+        return (Physics.Raycast(transform.position, Vector3.down, 1f));
+    }
+}
